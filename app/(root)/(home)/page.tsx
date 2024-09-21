ff08ee6e0401ece70b985002b4149af64328be4b
nhlinhseuit <dev.hoanglinh@gmail.com>
2024-09-21 16:39:47 +0700

    implment base project - add auth

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,9 @@
+import "../../globals.css";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+    </main>
+  );
+}
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "@/app/globals.css";
+
+const AskQuestion = () => {
+  return (
+    <main>
+      <h1>AskQuestion</h1>
+    </main>
+  );
+};
+
+export default AskQuestion;
