5359f858467750c57cb905d782a9a748cb14c1e3
Khoadeptrai <96755096+dkdeptrai@users.noreply.github.com>
2024-10-03 20:54:46 +0700

    feat: finished footer component layout

--- components/shared/FooterComponent.tsx
+++ components/shared/FooterComponent.tsx
@@ -6,40 +6,71 @@ interface FooterComponentProps {
 };
 const FooterComponent: React.FC<FooterComponentProps> = (props) => {
   return (
-    <footer className={`${props.className}`}>
-      <div className="flex flex-col items-start w-auto gap-8 md:flex-row md:gap-88">
-        <div className="">khoa dep trai</div>
-        <div className="grid flex-grow grid-cols-1 gap-6 md:grid-cols-5">
-          <div className="flex flex-col min-w-24 gap-4">
+    <footer className={`${props.className} pb-16`}>
+      <div className="relative -top-20 h-auto w-full p-6 pb-0 bg-primary-500 rounded-[20px] -mt-24">
+        <div className="flex flex-col gap-8 md:flex-row md:justify-between">
+          <div className="flex flex-col justify-around">
+            <div className="text-5xl font-bold">Subscribe<br/>Newsletter</div>
+            <div className="opacity-80">The Travel</div>
+            <div className="opacity-80">
+              Get inspired! Received travel discounts, tips and behind the scenes stories.
+            </div>
+            <div className="flex flex-row justify-between gap-4 pb-4">
+              <input
+                type="text"
+                placeholder="Your email address"
+                className="flex-grow rounded-md border-none bg-white p-4 px-4 py-5 outline-none"
+              />
+              <button className="rounded-md bg-black px-4 py-5 text-white">
+                Subscribe
+              </button>
+            </div>
+          </div>
+
+          <img src="/assets/icons/mailbox.svg" alt="Mailbox" className="" />
+        </div>
+      </div>
+      <div className="flex w-auto flex-col items-start gap-8 md:flex-row md:gap-56">
+        <div className="flex flex-col items-center gap-8">
+          <img src="/assets/icons/footer-logo.svg" alt="Logo" className="w-full" />
+          <div className="flex flex-row gap-3">
+            <img src="/assets/icons/facebook.svg" alt="Facebook" className="w-6" />
+            <img src="/assets/icons/twitter.svg" alt="Twitter" className="w-6" />
+            <img src="/assets/icons/youtube.svg" alt="Youtube" className="w-6" />
+            <img src="/assets/icons/instagram.svg" alt="Instagram" className="w-6" />
+          </div>
+        </div>
+        <div className="grid flex-grow grid-cols-1 gap-8 md:grid-cols-5">
+          <div className="flex flex-col gap-4 min-w-24">
             <div className="mb-4 font-semibold">Our Destinations</div>
-            <div>Canada</div>
-            <div>Alaska</div>
-            <div>France</div>
-            <div>Iceland</div>
+            <a href="" className="opacity-70">Canada</a>
+            <a href="" className="opacity-70">Alaska</a>
+            <a href="" className="opacity-70">France</a>
+            <a href="" className="opacity-70">Iceland</a>
           </div>
-          <div className="flex flex-col min-w-24 gap-4">
+          <div className="flex flex-col gap-4 min-w-24">
             <div className="mb-4 font-semibold">Our Activities</div>
-            <div>Northern Lights</div>
-            <div>Cruising & Sailing</div>
-            <div>Multi-activities</div>
-            <div>Kayaking</div>
+            <a href="" className="opacity-70">Northern Lights</a>
+            <a href="" className="opacity-70">Cruising & Sailing</a>
+            <a href="" className="opacity-70">Multi-activities</a>
+            <a href="" className="opacity-70">Kayaking</a>
           </div>
-          <div className="flex flex-col min-w-24 gap-4">
+          <div className="flex flex-col gap-4 min-w-24">
             <div className="mb-4 font-semibold">Travel Blogs</div>
-            <div>Bali Travel Guide</div>
-            <div>Sri Lanka Travel Guide</div>
-            <div>Peru Travel Guide</div>
-            <div>Singapore Travel Guide</div>
+            <a href="" className="opacity-70">Bali Travel Guide</a>
+            <a href="" className="opacity-70">Sri Lanka Travel Guide</a>
+            <a href="" className="opacity-70">Peru Travel Guide</a>
+            <a href="" className="opacity-70">Singapore Travel Guide</a>
           </div>
-          <div className="flex flex-col min-w-24 gap-4">
-            <div className="mb-4 font-semibold">About Us</div>
-            <div>Our Story</div>
-            <div>Work with us</div>
+          <div className="flex flex-col gap-4 min-w-24">
+            <a  className="mb-4 font-semibold">About Us</a>
+            <a href="" className="opacity-70">Our Story</a>
+            <a href="" className="opacity-70">Work with us</a>
           </div>
-          <div className="flex flex-col min-w-24 gap-4">
+          <div className="flex flex-col gap-4 min-w-24">
             <div className="mb-4 font-semibold">Contact us</div>
-            <div>gogo-contacts@gogo.com</div>
-            <div>(123) 4456 4466</div>
+            <a href="" className="opacity-70">gogo-contacts@gogo.com</a>
+            <a href="" className="opacity-70">(123) 4456 4466</a>
           </div>
         </div>
       </div>
