bb5599282d1db464bff8c804e42dfeace2bbefc6
Khoadeptrai <96755096+dkdeptrai@users.noreply.github.com>
2024-12-12 14:04:49 +0700

    feat: updated amenities and image modal components to fit api response

--- types/Stay.ts
+++ types/Stay.ts
@@ -1,9 +1,11 @@
 import Location  from "./Location";
+import Image from "@/types/Image";
+import Amenity from "@/types/Amenity";
 interface Stay  {
 
   id: string;
   name: string;
-  amenities: string[];
+  amenities: Amenity[];
   address: string;
   location: Location
   rating: number;
@@ -13,6 +15,6 @@ interface Stay  {
   advantages: string[];
   star_rating: number;
   stay_type: "HOTEL" | "HOSTEL" | "RESORT" | "VILLA" | "APARTMENT" | string;
-  featured_images: string[];
+  featured_images: Image[];
 }
 export default Stay;
