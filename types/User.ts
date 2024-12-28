6a89391f0955f18da11b73479f35a79be3759a4e
Khoadeptrai <96755096+dkdeptrai@users.noreply.github.com>
2024-12-28 15:37:55 +0700

    fix: fixed User type

--- types/User.ts
+++ types/User.ts
@@ -1,12 +1,21 @@
+import Authority from "@/types/Authority";
+
 interface User {
   id: string;
   email: string;
   address: string;
+  enabled: boolean;
+  authorities: Authority[];
+  username: string;
+  accountNonExpired: boolean;
+  accountNonLocked: boolean;
   full_name: string;
   phone_number: string;
   date_of_birth: string;
   avatar_url: string;
   cover_url: string;
+  user_type: string;
+  is_deleted: boolean;
 }
 
 export default User;
