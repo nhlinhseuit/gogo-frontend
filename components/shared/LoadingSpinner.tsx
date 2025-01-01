073621c6f6dfad72da144f07ecd6a5ace9331c87
Nguyễn Hoàng Linh <93470801+nhlinhseuit@users.noreply.github.com>
2025-01-01 14:18:21 +0700

    api change password

--- components/shared/LoadingSpinner.tsx
+++ components/shared/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => {
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className="w-7 h-7 border-4 border-[#CDEAE1] border-t-[#8dd3bb] rounded-full animate-spin"></div>
+    <div className=" flex justify-center items-center h-full">
+      <div className="ml-1 mt-4 w-5 h-5 border-4 border-[#CDEAE1] border-t-[#8dd3bb] rounded-full animate-spin"></div>
     </div>
   );
 };
