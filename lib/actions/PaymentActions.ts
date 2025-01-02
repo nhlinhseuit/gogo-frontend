bf6670659df41bceffd642d9bcf5e57548443d79
Khoadeptrai <96755096+dkdeptrai@users.noreply.github.com>
2025-01-02 15:27:50 +0700

    feat: added payment after booking

--- lib/actions/PaymentActions.ts
+++ lib/actions/PaymentActions.ts
@@ -1,31 +1,37 @@
-import type Room from "@/types/Room";
+import Payment from "@/types/Payment";
 import {BASE_URL} from "@/constants";
 import {getToken} from "@/utils/util";
 import {handleError} from "@/lib/actions/HandleError";
 
-const API_URL = `${BASE_URL}/api/v1/rooms`
-const TEST_TOKEN = `Bearer ${process.env.NEXT_PUBLIC_TEST_TOKEN}`;
+const API_URL = `${BASE_URL}/api/v1`
 
-export const fetchRoom = async (roomId: string): Promise<Room> => {
+export const createPayment = async (cardId: string, bookingId: string, type: string): Promise<Payment> => {
   try {
-    const response = await fetch(`${API_URL}/${roomId}`, {
-      method: "GET",
+    const body = JSON.stringify({
+      cardId: cardId,
+      bookingId: bookingId,
+      type: type,
+    })
+    const response = await fetch(`${API_URL}/payment`, {
+      method: "POST",
       headers: {
         "Authorization": `Bearer ${getToken()}`,
         "Content-Type": "application/json",
       },
-    });
+      body: body
+    })
 
     if (!response.ok) {
       const errorData = await response.json();
       const apiError = errorData.apierror;
       handleError(apiError);
     }
+
     const data = await response.json();
-    return data.data as Room;
+    return data.data as Payment;
 
   } catch (error) {
-    console.error('Error fetching room:', error);
+    console.error('Error creating payment:', error);
     throw error;
   }
 }
